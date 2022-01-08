e6ceb72ff04ec6d8ffa842c8909bad9b147d92f3
PhilipShasha <philipshasha@hotmail.com>
2022-01-08 14:24:50 +1300

    React router initial configuration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+  useLocation,
+  BrowserRouter,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<PublicPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+
+function Layout() {
+  return (
+    <div>
+
+      <ul>
+        <li>
+          <Link to="/">Public Page</Link>
+        </li>
+        <li>
+          <Link to="/login">Protected Page</Link>
+        </li>
+      </ul>
+
     </div>
   );
 }
+
+function LoginPage() {
+  return (
+    <div>
+      <p>You must log in to view the page at </p>
+
+      <form>
+        <label>
+          Username: <input name="username" type="text" />
+        </label>{" "}
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+
+function PublicPage() {
+  return <h3>Public</h3>;
+}
+
 
 export default App;
